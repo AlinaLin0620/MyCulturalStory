e47f1f8b65b5b7965d99bc0d19827e685d6029cf
ALin0620 <alinalin0620@gmail.com>
2026-01-07 23:22:04 -0500

    zodiac animal class

--- src/myculturalstory/ZodiacAnimal.java
+++ src/myculturalstory/ZodiacAnimal.java
@@ -8,6 +8,31 @@ package myculturalstory;
  *
  * @author alina
  */
+import processing.core.PApplet;
+import processing.core.PImage;
 public class ZodiacAnimal {
+    // initialize varriables
+    protected PApplet app;
+    protected PImage image;
+    protected String name;
+    protected int x;
+    protected int y;
     
+    public ZodiacAnimal(PApplet app, String name, int x, int y) {
+        this.app = app;
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        
+        image = app.loadImage("images/" + name + "png");
+    }
+    
+    public void draw() {
+        app.image(image, x, y);
+    }
+    
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
 }
