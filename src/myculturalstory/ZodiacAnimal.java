82db5250d68b4c2f25a747197e8a722b69188cde
ALin0620 <alinalin0620@gmail.com>
2026-01-10 00:37:48 -0500

    change selection process plus implemented physics(jump)

--- src/myculturalstory/ZodiacAnimal.java
+++ src/myculturalstory/ZodiacAnimal.java
@@ -20,6 +20,14 @@ public class ZodiacAnimal {
     protected int x;
     protected int y;
     
+    // physics
+    protected float velocityY = 0;
+    protected final float GRAVITY = 0.8f;
+    protected final float JUMP_HEIGHT = -15;
+    protected boolean onGround = false;
+    protected final int GROUND_Y = 370;
+    
+    // zodiac animal constructor
     public ZodiacAnimal(PApplet app, String name, int x, int y) {
         this.app = app;
         this.name = name;
@@ -29,12 +37,41 @@ public class ZodiacAnimal {
         image = app.loadImage("images/" + name + ".png");
     }
     
+    // draw in images
     public void draw() {
         app.image(image, x, y);
     }
     
-    public void move(int dx, int dy) {
+    public void update() {
+        velocityY += GRAVITY;
+        y += velocityY;
+        
+        // stop at ground 
+        if (y >= GROUND_Y) {
+            y = GROUND_Y;
+            velocityY = 0;
+            onGround = true;
+        } else {
+            onGround = false;
+        }
+    }
+    
+    // jump method
+    public void jump() {
+        if (onGround) {
+            velocityY = JUMP_HEIGHT;
+            onGround = false;
+        }
+    }
+    public void move(int dx) {
         x += dx;
-        y += dy;
+    }
+    
+    public int getWidth() {
+        return image.width;
+    }
+    
+    public int getHeight() {
+        return image.height;
     }
 }
--- src/myculturalstory/ZodiacSketch.java
+++ src/myculturalstory/ZodiacSketch.java
@@ -14,10 +14,23 @@ import processing.core.PImage;
 
 public class ZodiacSketch extends PApplet {
     // initialize
+    // images
     private PImage gameImg;
     private PImage selectionImg;
+    private PImage leftTri;
+    private PImage rightTri;
+    // frames
     private int gameState = 0;
+    // player
     private ZodiacAnimal player;
+    // screen selction variables 
+    private int leftTriX = 150;
+    private int rightTriX = 800;
+    private int triY = 500;
+    // character selection
+    private ZodiacAnimal[] allAnimals;
+    private int selectedIndex = 0;
+    
     
     public static void main(String[] args) {
         PApplet.main("myculturalstory.ZodiacSketch");
@@ -30,10 +43,31 @@ public class ZodiacSketch extends PApplet {
     
     // set up background and player
     public void setup() {
+        // background images
         gameImg = loadImage("images/river_1.png");
         selectionImg = loadImage("images/characterSelection.png");
-        // default player as rat
-        player = new Rat(this, width / 2 - 86, height / 2 + 54);
+        // selection frame images
+        leftTri = loadImage("images/left_tri.png");
+        rightTri = loadImage("images/right_tri.png");
+        
+        // initialize animal all in order
+        allAnimals = new ZodiacAnimal[] {
+            new Rat(this, width / 2 - 86, height / 2 + 54),
+            new Ox(this, width / 2 - 59, height / 2 + 63),
+            new Tiger(this, width / 2 - 60, height / 2 + 71),
+            new Rabbit(this, width / 2 - 69, height / 2 + 53),
+            new Dragon(this, width / 2 - 52, height / 2 + 50),
+            new Snake(this, width / 2 - 53, height / 2 + 73),
+            new Horse(this, width / 2 - 53, height / 2 + 52),
+            new Sheep(this, width / 2 - 58, height / 2 + 43),
+            new Monkey(this, width / 2 - 75, height / 2 + 75),
+            new Rooster(this, width / 2 - 47, height / 2 + 53),
+            new Dog(this, width / 2 - 76, height / 2 + 73),
+            new Pig(this, width / 2 - 61, height / 2 + 59)        };
+        
+        // default player
+        selectedIndex = 0;
+        player = allAnimals[selectedIndex];
     }
     
     // draw in frames
@@ -54,15 +88,15 @@ public class ZodiacSketch extends PApplet {
         fill(0);
         textSize(40);
         text("Choose your zodiac Animal", width / 2 - 215, 70);
-        
         textSize(20);
-        text("1-Rat  2-Ox  3-Tiger  4-Rabbit  5-Dragon  6-Snake\n" +
-             "7-Horse  8-Sheep  9-Monkey  Q-Rooster  W-Dog  E-Pig\n" +
-             "Press Enter to confirm", 290, 120);
+        text("Press Enter to confirm selection", 370, 120);
         
-        if (player != null) {
-            player.draw();
-        }
+        // draw triangles
+        if (leftTri != null) image(leftTri, leftTriX, triY);
+        if (rightTri != null) image(rightTri, rightTriX, triY);
+        
+        // draw player character
+        if (player != null) player.draw();
     }
     
     // game screen / frame
@@ -74,44 +108,70 @@ public class ZodiacSketch extends PApplet {
         
         // draw player 
         if (player != null) {
+            player.update();
             player.draw();
         }
         
         // player movement instructions
         fill(0);
         textSize(12);
-        text("Press Arrow Keys to Move", 10, 20);
+        text("Press Arrow Keys to Move and Space to Jump", 10, 20);
     }
     
+    private boolean isClicked(PImage img, int imgX, int imgY) {
+        return mousePressed && mouseX >= imgX &&mouseX <= imgX + img.width && mouseY >= imgY && mouseY <= imgY + img.height;
+    }
     // key controls
     public void keyPressed() {
         // player selection
         if (gameState == 0) {
-            if (key == '1') player = new Rat(this, width / 2 - 86, height / 2 + 54);
-            else if (key == '2') player = new Ox(this, width / 2 - 59, height / 2 + 63);
-            else if (key == '3') player = new Tiger(this, width / 2 - 60, height / 2 + 71);
-            else if (key == '4') player = new Rabbit(this, width / 2 - 69, height / 2 + 53);
-            else if (key == '5') player = new Dragon(this, width / 2 - 52, height / 2 + 50);
-            else if (key == '6') player = new Snake(this, width / 2 - 53, height / 2 + 73);
-            else if (key == '7') player = new Horse(this, width / 2 - 53, height / 2 + 52);
-            else if (key == '8') player = new Sheep(this, width / 2 - 58, height / 2 + 43);
-            else if (key == '9') player = new Monkey(this, width / 2 - 75, height / 2 + 75);
-            else if (key == 'q' || key == 'Q') player = new Rooster(this, width / 2 - 47, height / 2 + 53);
-            else if (key == 'w' || key == 'W') player = new Dog(this, width / 2 - 76, height / 2 + 73);
-            else if (key == 'e' || key == 'E') player = new Pig(this, width / 2 - 61, height / 2 + 59);    
-            
             // change frame
             if (keyCode == ENTER && player != null) {
                 gameState = 1;
+                
+                // new start position for player 
+                int START_X = 100;
+                int START_Y = 370;
+                
+                // center the image horizontally 
+                player.x = START_X - player.getWidth() / 2;
+                // place animal vetically so image touches ground
+                player.y = START_Y - player.getHeight();
             }
         }
         
         // enable movement in new frame
         else if (gameState == 1 && player != null) {
-            if (keyCode == LEFT) player.move(-5, 0);
-            else if (keyCode == RIGHT) player.move(5, 0);
-            else if (keyCode == UP) player.move(0, -5);
-            else if (keyCode == DOWN) player.move(0, 5);
+            // move left and right 
+            if (keyCode == LEFT) {
+                player.move(-5);
+            }
+            else if (keyCode == RIGHT) {
+                player.move(5);
+            }
+            
+            // jump 
+            if (key == ' ') {
+                player.jump();
+            }
         }
-    }   
+    }
+    
+    // mouse controls
+    public void mousePressed() {
+        if (gameState == 0) {
+            // check left tri
+            if (isClicked(leftTri, leftTriX,triY)) {
+                selectedIndex--;
+                if (selectedIndex < 0) selectedIndex = allAnimals.length - 1;
+                player = allAnimals[selectedIndex];
+            }
+            // check right tri
+            else if (isClicked(rightTri, rightTriX, triY)) {
+                selectedIndex++;
+                if (selectedIndex >= allAnimals.length) selectedIndex = 0;
+                player = allAnimals[selectedIndex];
+            }
+        }
+    }
 }
